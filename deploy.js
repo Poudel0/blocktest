65ca55f75efe5c668bda495b4329550dd8e7de79
Poudel0 <poudel0sandesh@gmail.com>
2022-12-30 21:44:31 +0545

    Live contract deployment

--- deploy.js
+++ deploy.js
@@ -4,9 +4,9 @@ const fs = require("fs-extra");
 async function main() {
   //http://127.0.0.1:8545
   const provider = new ethers.providers.JsonRpcProvider(
-    "//http://127.0.0.1:8545"
+    "http://127.0.0.1:8545"
   );
-  const walle = new ethers.Wallet(
+  const wallet = new ethers.Wallet(
     "0xf9c093d5a60ccff3ec5634ad1bb8b129454287ffbb13bb14cd38afaf6284475f",
     provider
   );
@@ -14,6 +14,11 @@ async function main() {
   const abi = fs.readFileSync("_first file_sol_SimpleStorage.abi", "utf-8");
 
   const binary = fs.readFileSync("_first file_sol_SimpleStorage.bin", "utf-8");
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying the first contract");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
