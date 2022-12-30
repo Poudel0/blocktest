b953fc27303c59c60b0f614b4ff3f5878d1b02d8
Poudel0 <poudel0sandesh@gmail.com>
2022-12-30 21:09:36 +0545

    This is the continuiationnnnn

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  //http://127.0.0.1:8545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "//http://127.0.0.1:8545"
+  );
+  const walle = new ethers.Wallet(
+    "0xf9c093d5a60ccff3ec5634ad1bb8b129454287ffbb13bb14cd38afaf6284475f",
+    provider
+  );
+
+  const abi = fs.readFileSync("_first file_sol_SimpleStorage.abi", "utf-8");
+
+  const binary = fs.readFileSync("_first file_sol_SimpleStorage.bin", "utf-8");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
