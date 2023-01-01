0ac419804d15f2f0458b141d7a5eb8443aa4979f
Poudel0 <poudel0sandesh@gmail.com>
2023-01-01 22:42:08 +0545

    no more oopsie

--- deploy.js
+++ deploy.js
@@ -3,10 +3,14 @@ const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function main() {
-  //http://127.0.0.1:8545
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
   const abi = fs.readFileSync("_first file_sol_SimpleStorage.abi", "utf-8");
 
   const binary = fs.readFileSync("_first file_sol_SimpleStorage.bin", "utf-8");
